0b6a5728e1f2e8e356ab6ca9bf8cb1565e55a1c7
Jon Tamir <jtamir@utexas.edu>
2021-01-12 01:31:53 -0600

    rf_seq: fix missing theta

--- bloch/rf_seq.py
+++ bloch/rf_seq.py
@@ -42,6 +42,8 @@ def sinc_pulse(timebandwidth, flip_angle, duration, dt, gamma=26747.52):
     Output:
     rf: Sinc pulse rf signal
     """
+    samples = int(duration / dt)
+    theta = np.linspace(-timebandwidth/2, timebandwidth/2, samples+2)
     #### Original Code: wrong RF Pulse ####
     # rf = np.sinc(theta[1:-1] * sig.hann(samples))
     # rf = flip_angle * (rf/np.sum(rf))
